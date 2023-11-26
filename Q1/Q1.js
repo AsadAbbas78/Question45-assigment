d5132792733861d8ba4ece3918088f5643915682
AsadAbbas <assadgamerchannel1@gmail.com>
2023-11-26 04:21:25 -0800

    first commit

---
+++ Q1/Q1.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let Assad = console.log("Hello Assad, would you like to learn some python");
---
+++ Q1/Q1.ts
@@ -0,0 +1,1 @@
+let Assad = console.log("Hello Assad, would you like to learn some python");
